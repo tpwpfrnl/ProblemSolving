7a52ffcf5015c6a6e9c7ee9a50f61d2346edd568
Youngchae Kwon <21800033@handong.edu>
2023-01-19 21:07:24 +0900

    230119

--- a100.py
+++ a100.py
@@ -4,6 +4,12 @@ Problem : https://leetcode.com/problems/range-sum-of-bst/description/
 Date : 20230119
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         next = None
